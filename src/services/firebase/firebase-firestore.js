2920c314a4617b5c09a500ecf3e1f78f45084059
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-20 18:10:52 +0300

    firebase-messaging && firebase-firestore

--- src/services/firebase/firebase-firestore.js
+++ src/services/firebase/firebase-firestore.js
@@ -1,1 +1,62 @@
-import {} from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  doc,
+  getFirestore,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { firebaseApp } from "./firebase-config";
+
+const firestore = getFirestore(firebaseApp);
+
+export const saveDocData = async (docName, docData) => {
+  try {
+    return await addDoc(collection(firestore), docName, {
+      ...docData,
+      timestamp: serverTimestamp(),
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loadDocData = async (docName, limitNo = 12) => {
+  try {
+    const recentQuery = query(
+      collection(firestore),
+      docName,
+      orderBy("timestamp", "desc"),
+      limit(limitNo)
+    );
+    onSnapshot(recentQuery, (snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === "removed") {
+          // delete doc
+        } else {
+          const docData = change.doc;
+          return docData;
+        }
+      });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const setDocData = async (docName, docData, otherData = {}) => {
+  try {
+    const docRef = doc(firestore, docName, docData);
+    return await setDoc(docRef, { ...otherData, timestamp: serverTimestamp() });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateDocData = async(docName,docData, otherData={})=>{
+    
+}
--- src/services/firebase/firebase-messaging.js
+++ src/services/firebase/firebase-messaging.js
@@ -1,1 +1,39 @@
-import {} from 'firebase/messaging';
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { firebaseApp } from "./firebase-config";
+
+const messaging = getMessaging(firebaseApp);
+
+// Saving the messaging device token to Cloud Firestore.
+const savaMessagingDeviceToken = async () => {
+  try {
+    const currentToken = await getToken(messaging);
+    if (currentToken) {
+      console.log("Got FCM  device token.");
+      // get ref
+      // set doc
+
+      onMessage(messaging, (message) => {
+        // notification payload
+        console.log(
+          "New foreground notification from Firebase Messaging!",
+          message.notification
+        );
+      });
+    } else {
+      await requestNotificationPermissions();
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const requestNotificationPermissions = async () => {
+  console.log("Requesting notifications permission...");
+  const permission = await Notification.requestPermission();
+  if (permission) {
+    console.log("Notification permission granted.");
+    await savaMessagingDeviceToken();
+  } else {
+    console.log("Unable to get permission to notify.");
+  }
+};
