2058cc84beb1f65d08a3d737326dd8ea551044f5
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-21 18:02:30 +0300

    user firestore

---
+++ src/services/firebase/model/user-firebase.js
@@ -0,0 +1,60 @@
+import {
+  deleteDocData,
+  loadDocDataById,
+  loadDocsData,
+  saveDocData,
+  updateDocData,
+} from "../firebase-firestore";
+
+const docName = "users";
+export const saveUserFirebase = async (user = {}) => {
+  try {
+    const { firstName, lastName, email } = user;
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+    return await saveDocData(docName, "", userData);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUsersFirebase = async () => {
+  try {
+    return await loadDocsData(docName);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getUserFirebase = async (userId) => {
+  try {
+    return await loadDocDataById(docName, userId);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateUserFirebase = async (userId, user) => {
+  try {
+    const { firstName, lastName, email } = user;
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+    return await updateDocData(docName, userId, "", userData);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteUserFirebase = async (userId) => {
+  try {
+    return await deleteDocData(docName, userId);
+  } catch (error) {
+    throw error;
+  }
+};
