33d8dc6f867cf2df64e3e80bc06dc6a11d87d30c
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2024-02-21 08:45:39 +0300

    user auth

--- src/services/firebase/firebase-auth.js
+++ src/services/firebase/firebase-auth.js
@@ -12,12 +12,20 @@ const auth = getAuth(firebaseApp);
 // Sign-in PoV in Firebase using popup auth and Google as the identity provider.
 const signInWithGoogleAUth = async () => {
   const provider = new GoogleAuthProvider();
-  await signInWithPopup(auth, provider);
+  try {
+    await signInWithPopup(auth, provider);
+  } catch (error) {
+    throw error;
+  }
 };
 
 // sign-out of PoV
 const signOutUser = async () => {
-  await signOut(auth);
+  try {
+    await signOut(auth);
+  } catch (error) {
+    throw error;
+  }
 };
 
 // Returns true if a user is signed-in.
